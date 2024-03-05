05f1ef2a259968ed87d914796de6e59c274ef3eb
Simon Cropp <simon.cropp@gmail.com>
2024-03-06 09:43:35 +1100

    .

--- src/Verify.DocNet/GlobalUsings.cs
+++ src/Verify.DocNet/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using Docnet.Core;
 global using Docnet.Core.Converters;
+global using Docnet.Core.Models;
 global using Docnet.Core.Readers;
 global using SixLabors.ImageSharp;
 global using SixLabors.ImageSharp.PixelFormats;
